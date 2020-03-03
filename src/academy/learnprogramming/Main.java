4b96cc4d0172831b01a48ae95bc03b660df2923a
Carmen <Carmen.smith490@gmail.com>
2020-03-03 11:05:06 -0500

    Dragon Cave Completed

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -1,8 +1,33 @@
 package academy.learnprogramming;
 
+
+
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner option = new Scanner(System.in);
+
+        System.out.println("Enter a number, any number, be careful, you are choosing your fate!!!");
+
+      int Option1 = option.nextInt();
+      if(Option1 == 1){
+        System.out.println("You are currently approaching the cave, its dark, it SPOOKY!!!"  + "\n" +
+                "A large dragon jumps out in front of you....!!!!!" + "\n" + "Oh no he opens his jaws!!!!!!!!" + "\n" +
+                "He gobbles you in one bite... OH NO!!!" +
+                "\n" + "*****************GAME OVER ******************");
+    }
+
+      else {
+
+          if (Option1 == 2){
+              System.out.println("You made it safe from the dragon!! Good job!");
+          }
+
+      }
+
+
     }
 }
